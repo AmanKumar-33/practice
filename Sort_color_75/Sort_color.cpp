84183d5305f325bf952a071d73faa07645bbe077
aman singh <amansingh9876545@gmail.com>
2023-12-14 22:26:08 +0530

    Sort_color repo is added

---
+++ Sort_color_75/Sort_color.cpp
@@ -0,0 +1,29 @@
+// writing the program for the problem.
+#incllude<bits/stdc++.h>
+using namespace std;
+void Sort_color(vector<int>&nums){
+    int zero=0,one=0,two=0;
+    int n = nums.size();
+    for(int i=0;i<n;i++){
+        if(nums[i]==0){
+            zero++;
+        }
+        else if(nums[i]==1){
+            one++;
+        }
+        else{
+            two++;
+        }
+    }
+    int i =0;
+    while(zero--){
+        nums[i++]=0;
+    }
+    while(one--){
+        nums[i++]=1;
+    }
+    while(two--){
+        nums[i++]=2;
+    }
+
+}
